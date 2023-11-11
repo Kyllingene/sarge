ca3a6de395077669b5829d6b78f2413fa5ed7c26
Kyllingene <fuzzymuffin343@gmail.com>
2023-11-11 10:50:54 -0800

    Switching to master

--- sarge-macros/src/implement.rs
+++
@@ -1,9 +0,0 @@
-impl crate::Arguments for {{NAME}} {
-    fn new() -> Self {
-        todo!()
-    }
-
-    fn parse_args(&mut self, args: &[String]) -> Result<(), crate::ArgParseError> {
-        todo!()
-    }
-}
--- sarge-macros/src/lib.rs
+++ sarge-macros/src/lib.rs
@@ -1,13 +1,26 @@
 use proc_macro::{TokenTree, TokenStream};
 
+const IMPLEMENTATION: &str = r##"
+impl crate::Arguments for {{NAME}} {
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn parse_args(&mut self, args: &[String]) -> Result<(), crate::ArgParseError> {
+        todo!()
+    }
+}
+"##;
+
 #[derive(PartialEq)]
 enum State {
     Initial,
     Name,
     Parameters,
     InParam {
+        public: bool,
         name: String,
-        typ: String,
+        typ: Option<String>,
         short: Option<char>,
     },
     Complete
@@ -15,6 +28,7 @@ enum State {
 
 #[derive(PartialEq)]
 struct Param {
+    public: bool,
     name: String,
     typ: String,
     short: Option<char>,
@@ -31,6 +45,10 @@ struct Param {
             #[short = 'f']
             fourth: f64,
         }
+
+        struct : Initial -> Name
+        Args : Name -> Parameters
+        first: Parameters -> InParam
 */
 
 #[proc_macro_derive(Arguments, attributes(short))]
@@ -40,7 +58,7 @@ pub fn derive_arguments_struct(item: TokenStream) -> TokenStream {
     let mut state = State::Initial;
 
     let mut name: Option<String> = None;
-    let mut current: Option<Param> = None;
+    // let mut current: Option<Param> = None;
     let mut params: Vec<Param> = Vec::new();
 
     for tok in toks {
@@ -49,7 +67,11 @@ pub fn derive_arguments_struct(item: TokenStream) -> TokenStream {
             TokenTree::Ident(i) => {
                 let ident = i.to_string();
                 match ident.as_str() {
-                    "pub" => continue,
+                    "pub" => match state {
+                        State::Initial => continue,
+                        State::Parameters => state = State::InParam { public: true, name: String::new(), typ: None, short: None },
+                        _ => panic!("Unexpected token: `pub`"),
+                    },
                     "struct" => {
                         if state != State::Initial {
                             panic!("Unexpected identifier: `{ident}`");
@@ -59,23 +81,36 @@ pub fn derive_arguments_struct(item: TokenStream) -> TokenStream {
                         continue;
                     },
                     _ => {
-                        match state {
+                        match &mut state {
                             State::Initial => panic!("Unexpected identifier: `{ident}`"),
                             State::Name => {
-                                state = State::Name;
+                                state = State::Parameters;
                                 name = Some(ident);
                                 continue;
                             },
                             State::Parameters => {
-                                current = Some(Param { name: ident, typ: String::new(), short: None });
+                                state = State::InParam { public: false, name: ident, typ: None, short: None };
+                                // current = Some(Param { name: ident, typ: String::new(), short: None });
+                                continue;
                             }
-                            State::InParam { name, typ, short } => todo!(),
+                            State::InParam { typ, .. } => {
+                                *typ = Some(ident);
+                            }
                             State::Complete => panic!("Internal error: tried to parse more after State::Complete"),
                         }
                     }
                 }
             },
-            TokenTree::Punct(_) => {},
+            TokenTree::Punct(punct) => match punct.as_char() {
+                '}' => match state {
+                    State::Parameters => {
+                        state = State::Complete;
+                        break;
+                    }
+                    _ => panic!("Unexpected token: `}}`"),
+                },
+                _ => {}
+            },
             TokenTree::Literal(_) => {},
         }
     }
@@ -84,6 +119,6 @@ pub fn derive_arguments_struct(item: TokenStream) -> TokenStream {
         panic!("Internal error: state != Complete");
     }
 
-    let new = include_str!("implement.rs").to_string().replace("{{NAME}}", &name.expect("Expected name for struct"));
+    let new = IMPLEMENTATION.to_string().replace("{{NAME}}", &name.expect("Expected name for struct"));
     new.parse().unwrap()
 }
